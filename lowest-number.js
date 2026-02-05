373747e76e1f263782612323edea845633ad8b5f
musabbirrahman <musabbirrahman490@gmail.com>
2026-02-05 18:47:23 +0600

    created function to find lowest number

--- lowest-number.js
+++ lowest-number.js
@@ -1,3 +1,16 @@
 // Find the lowest number in the array below.
 
-const heights2 = [167, 190, 120, 165, 137];
+const heights = [167, 190, 120, 165, 137];
+
+function getLowestNumber(numbers){
+    let min = numbers[0];
+    for(let number of numbers){
+        if(number < min){
+            min = number;
+        }
+    }
+    return min;
+}
+
+const lowestNumber = getLowestNumber(heights);
+console.log(lowestNumber);
